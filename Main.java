e342ae5ec43f603481001459128a300409e470e7
Ebtehal Alfaifi <ebtehal.alfaifi@outlook.sa>
2024-10-23 21:25:49 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,151 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        Scanner eb = new Scanner(System.in);
+
+//        1.Write a program that prints the numbers from 1 to 100 such that:
+//        If the number is a multiple of 3, you need to print "Fizz" instead of that
+//        number.
+
+
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 3 == 0 && i % 5 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println(i);
+//            }
+
+
+        //2.Write a Java program to reverse a string.
+        //Test Data: Input a string: The quick brown fox Expected Output: Reverse
+        //string: xof nworb kciuq ehT
+
+
+//            System.out.println("enter sentence that you want to reverce");
+//            String sentence = eb.nextLine();
+//            String reversed = new StringBuilder(sentence).reverse().toString();
+//            System.out.println("Reverse string: " + reversed);
+//        }
+
+
+//        3.Write a program to find the factorial value of any number entered
+//        through the keyboard.
+
+
+//        System.out.println("Enter a number: ");
+//        int num = eb.nextInt();
+//        long factorial = 1;
+//
+//        for (int i = 1; i <= num; i++) {
+//            factorial *= i;
+//            System.out.println(factorial);
+//        }
+
+
+//        4.Two numbers are entered through the keyboard. Write a program to find
+//        the value of one number raised to the power of another. (Do not use Java
+//        built-in method)
+
+
+//        System.out.print("Enter the base number: ");
+//        int base = eb.nextInt();
+//        System.out.print("Enter the exponent number: ");
+//        int expo = eb.nextInt();
+//
+//        int result = 1;
+//        for (int i = 0; i < expo; i++) {
+//            result *= base;
+//        }
+//
+//        System.out.println(base + " raised to the power of " + exponent + " is " + result);
+
+
+
+//        5.Write a program that reads a set of integers, and then prints the sum of
+//        the even and odd integers.
+
+//
+//  int number;
+//  int even = 0;
+//  int odd = 0;
+//  do {
+//  System.out.println("enter number or 0 to exite");
+//  number = eb.nextInt();
+//  if (number % 2 == 0) {
+//  even = even + number;
+//  } else odd = odd + number;
+//  }while (number!=0);
+//  System.out.println("Sum of even integers: " + even);
+//  System.out.println("Sum of odd integers: " + odd);
+//   }}
+
+
+
+//      6.  Write a program that prompts the user to input a positive integer. It
+//        should then output a message indicating whether the number is a prime
+//        number.
+
+
+//System.out.println("Enter a positive number: ");
+//int number = eb.nextInt();
+//if (number <= 1) {
+// System.out.println(number + " is not a prime number.");
+//} else {
+//int i;
+// for (i = 2; i * i <= number; i++) {
+//  if (number % i == 0) {
+//   break;
+//}}
+//if (i * i > number) {
+//System.out.println(number + " is a prime number.");
+// } else {
+// System.out.println(number + " is not a prime number.");
+//   }}}}
+
+
+
+
+//        7.Use a for loop to print headings for four weeks (Weeks 1 - 4). Then use
+//        another for loop to print the days (Days 1 -7) for each week.
+
+//  for (int week = 1; week <= 4; week++) {
+//  System.out.println("Week " + week);
+// for (int day = 1; day <= 7; day++) {
+//   System.out.println("Day " + day);
+//            }
+//        }
+//    }
+//}
+
+
+
+
+
+
+
+//        8.Write a program thats check if the word is a palindrome or not. hint: A
+//        string is said to be a palindrome if it is the same if we start reading it from
+//        left to right or right to left.
+
+// System.out.print("Enter a string: ");
+//String e= eb.nextLine();
+//String reversed = new StringBuilder(e).reverse().toString();
+//if (e.equals(reversed)) {
+//System.out.println(e + " is a palindrome.");
+// } else {
+// System.out.println(e + " is not a palindrome.");
+// }}}
+
+
+
+
